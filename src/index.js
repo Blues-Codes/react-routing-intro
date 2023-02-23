b1d8c2012a6fb1017c6924c5d1e77035ad774b0e
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-02-23 16:40:35 -0500

    in lesson lab created

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom'; // <== IMPORT
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Next, wrap the <App /> with the <Router> and </Router> tags
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>                            
+      <App />
+    </BrowserRouter>                      
   </React.StrictMode>
 );
+
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
